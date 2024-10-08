cacc9ac6787c8b5f6aed88e30dc82bf4da48281c
Lycoriste <steventeng03@gmail.com>
2024-10-07 23:42:01 -0700

    Added BGSelector.tsx

---
+++ src/components/BGSelector.tsx
@@ -0,0 +1,24 @@
+type ContactProps = {
+    current: number;
+    set: React.Dispatch<React.SetStateAction<number>>;
+}
+
+
+export const BGSelector: React.FC<ContactProps> = ({ current, set }) => {
+    return (
+        <section className="background-button-selection">
+            <button className={`background-button`} onClick={() => { set(0) }}>
+                <div className={`flex w-full h-full ${current == 0 ? 'bg-white' : ''}`} />
+            </button>
+            <label className="background-button-label">BLANK</label>
+            <button className={`background-button`} onClick={() => { set(1) }}>
+                <div className={`flex w-full h-full ${current == 1 ? 'bg-white' : ''}`} />
+            </button>
+            <label className="background-button-label">BASIC</label>
+            <button className={`background-button`} onClick={() => { set(2) }}>
+                <div className={`flex w-full h-full ${current == 2 ? 'bg-white' : ''}`} />
+            </button>
+            <label className="background-button-label">DETAILED</label>
+        </section>
+    );
+}
