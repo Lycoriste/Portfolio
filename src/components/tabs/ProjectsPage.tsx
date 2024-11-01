b6a760858d5b1b1983fb76c147ae5de79318761d
Lycoriste <steventeng03@gmail.com>
2024-11-01 15:18:52 -0700

    Project cards skeleton

--- src/components/tabs/ProjectsPage.tsx
+++ src/components/tabs/ProjectsPage.tsx
@@ -7,6 +7,18 @@ export const ProjectsPage: React.FC<ProjectsProps> = ({ current }) => {
         <div id='projects-content' className={`${current == 'Projects' ? 'flex opacity-100' : 'hidden opacity-0'}`}>
             <span id='projects-header'>Projects</span>
             <div id='project-scroller'>
+                <div id='chrome-extension-project' className="project-card">
+
+                </div>
+                <div id='ai-flashcard-project' className="project-card">
+
+                </div>
+                <div id='roblox-ai-project' className="project-card">
+
+                </div>
+                <div id='placeholder-project' className="project-card">
+
+                </div>
             </div>
         </div>
     )
