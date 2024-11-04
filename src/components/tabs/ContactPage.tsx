ced038d63e89ec1ea10a4bdd5d487891aa8090f6
Lycoriste <steventeng03@gmail.com>
2024-11-04 00:35:11 -0800

    Temporary contact

---
+++ src/components/tabs/ContactPage.tsx
@@ -0,0 +1,17 @@
+type AboutProps = {
+    current: string;
+}
+
+export const ContactPage: React.FC<AboutProps> = ({ current }) => {
+    return (
+        <div id='contact-container' className={`${current == 'Contact' ? 'flex opacity-100' : 'hidden opacity-0'}`}>
+            <span id='contact-body'>
+                <span id='contact-header'>Contact</span>
+                steventeng03@gmail.com <br />
+                nhteng@ucsd.edu <br />
+                <a href='https://github.com/Lycoriste/'>https://github.com/Lycoriste</a>
+                <a href='https://www.linkedin.com/in/lycoriste/'>https://www.linkedin.com/in/lycoriste</a>
+            </span>
+        </div>
+    )
+}
--- src/components/tabs/ProjectsPage.tsx
+++ src/components/tabs/ProjectsPage.tsx
@@ -5,7 +5,7 @@ type ProjectsProps = {
 export const ProjectsPage: React.FC<ProjectsProps> = ({ current }) => {
     return (
         <div id='projects-content' className={`${current == 'Projects' ? 'flex opacity-100' : 'hidden opacity-0'}`}>
-            <span id='projects-header'>Projects</span>
+            <span id='projects-header'>Projects (ADDING SOON)</span>
             <div id='project-scroller'>
                 <div id='chrome-extension-project' className="project-card">
 
